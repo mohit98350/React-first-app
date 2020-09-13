e1241af18dd4269fe5e80ea23df62b53048ea5c5
Mohit Mayank <65847302+mohit98350@users.noreply.github.com>
2020-09-14 00:20:30 +0530

    Updates

---
+++ precache-manifest.a0f7d51eefffc3efa890fc6ad5bf7ea8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aed4f1fb6d865d988cef383ba7e6fd9",
+    "url": "/React-first-app/index.html"
+  },
+  {
+    "revision": "e7f03f2723709554ddc6",
+    "url": "/React-first-app/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f1a93b22fe371bd52aee",
+    "url": "/React-first-app/static/css/main.08325f40.chunk.css"
+  },
+  {
+    "revision": "e7f03f2723709554ddc6",
+    "url": "/React-first-app/static/js/2.e8c067bf.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-first-app/static/js/2.e8c067bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a93b22fe371bd52aee",
+    "url": "/React-first-app/static/js/main.71327364.chunk.js"
+  },
+  {
+    "revision": "17bbab2779852d2f4460",
+    "url": "/React-first-app/static/js/runtime-main.f7326db6.js"
+  },
+  {
+    "revision": "6f6fb61f8d63ff1da125dfa51894ad35",
+    "url": "/React-first-app/static/media/ANDROID.6f6fb61f.gif"
+  },
+  {
+    "revision": "e65751d1a8bad22de2a682e905439012",
+    "url": "/React-first-app/static/media/APP.e65751d1.png"
+  },
+  {
+    "revision": "14b230eb489bb2eba2685fec486ff8be",
+    "url": "/React-first-app/static/media/MARKETING.14b230eb.jpg"
+  },
+  {
+    "revision": "53b3417f2cd89f66e922e62972a09224",
+    "url": "/React-first-app/static/media/SOFTWARE.53b3417f.jpg"
+  },
+  {
+    "revision": "3541309b4912ed1eea86229e16f24984",
+    "url": "/React-first-app/static/media/about.3541309b.png"
+  },
+  {
+    "revision": "0efa8a9aa597b0f6ca8736f983572d26",
+    "url": "/React-first-app/static/media/dev2.0efa8a9a.jpg"
+  },
+  {
+    "revision": "c722c0513ab109cdc6f0819e3a245930",
+    "url": "/React-first-app/static/media/developer.c722c051.jpg"
+  },
+  {
+    "revision": "d1c52627416d6976602d79eb749301cc",
+    "url": "/React-first-app/static/media/digital.d1c52627.jpg"
+  }
+]);
